6a1f3bcfaf7e3fff93ba03b08357bfe2ae4f66eb
mudasir-007 <mudasirwani4567@gmail.com>
2026-09-17 10:36:29 +0530

    Add compliance tests

---
+++ backend/test_compliance.py
@@ -0,0 +1,6 @@
+from src.compliance_checklist import ComplianceChecker
+
+checker = ComplianceChecker()
+sample = "The project has obtained building permit and fire NOC. soil test was conducted."
+
+print(checker.checkz)
